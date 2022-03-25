72308c882e057fff80344362ffe9a93026614abf
Magzhan Istemesov <istemesov.magzhan@mail.ru>
2022-03-25 10:50:04 +0600

    add css

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
       <Paragraph size="14px">This is sample Paragraph</Paragraph>
       <Paragraph size=".5rem">This is sample Paragraph</Paragraph>
       <Paragraph color="red" size="3rem">This is sample Paragraph</Paragraph>
-      <Image url={koalaImage} width="150px" height="100px"/>
+      <Image align="left" url={koalaImage} width="150px" height="100px"/>
+      <Image align="right" url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Cutest_Koala.jpg/200px-Cutest_Koala.jpg" width="150px" height="100px"/>
     </div>
   );
 }
